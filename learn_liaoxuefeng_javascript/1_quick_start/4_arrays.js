946ddf895d82524ca7d380b76af7688ed111c223
henan-wq <nanhe31@gmail.com>
2022-05-18 20:11:33 +0100

    modified:   4_arrays.js

--- learn_liaoxuefeng_javascript/1_quick_start/4_arrays.js
+++ learn_liaoxuefeng_javascript/1_quick_start/4_arrays.js
@@ -13,3 +13,20 @@ arr; // (2) [1, 2]
 arr.length = 3;
 arr; // (3) [1, 2, empty]
 
+// Array 可以通过索引把对应的元素修改为新的值，因此对Array的索引进行赋值会改变这个Array：
+var arr = ['A', 'B', 'C'];
+arr[1] = 99;
+arr; // (3) ['A', 99, 'C']
+// 通过索引赋值时，索引超过了范围同样会改变这个Array：
+arr[5] = 'x';
+arr; // (6) ['A', 99, 'C', empty × 2, 'x']
+// 在编写代码时，不建议直接修改Array的大小，访问索引时要确保索引不会越界。
+
+// Array 通过 indexOf() 来搜索一个指定的元素的位置：
+var arr = [10, 20, '30', 'xyz'];
+arr.indexOf(10); // 0
+arr.indexOf(20); // 1
+arr.indexOf('30'); // 2
+arr.indexOf('xyz'); // 3
+arr.indexOf(NaN); // 试了以下居然是-1
+arr.indexOf(0); // -1 没找到的返回-1
