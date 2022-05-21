8449480265b5a0ca8a52ad3325fbbf550c59f6bf
henan-wq <nanhe31@gmail.com>
2022-05-21 16:30:56 +0100

    modified:   7_for.js

--- learn_liaoxuefeng_javascript/1_quick_start/7_for.js
+++ learn_liaoxuefeng_javascript/1_quick_start/7_for.js
@@ -69,3 +69,24 @@ name
 age
 city
 */
+
+// 过略掉对象继承的属性，用hasOwnPoperty()来实现：
+var o = {
+    name: 'Jack',
+    age:20,
+    ciy: 'Beijing'
+};
+for (var key in o) {
+    if (o.hasOwnProperty(key)) {
+        console.log(key); // 'name', 'age', 'city'
+    }
+}
+
+/* 由于Array也是对象，而它的每个元素的索引被视为对象的属性，因此，
+for ... in循环可以直接循环出Array的索引：
+*/
+var a = ['A', 'B', 'C'];
+for (var i in a) {
+    console.log(i); // '0', '1', '2'
+    console.log(a[i]); // 'A', 'B', 'C'
+}
