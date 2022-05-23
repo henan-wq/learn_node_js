b856a7a3d3c63a9ac1394842147576d58877b78a
henan-wq <nanhe31@gmail.com>
2022-05-23 08:03:22 +0100

    modified:   1_function_definitions_and_call.js

--- learn_liaoxuefeng_javascript/2_function/1_function_definitions_and_call.js
+++ learn_liaoxuefeng_javascript/2_function/1_function_definitions_and_call.js
@@ -135,5 +135,49 @@ function foo(a, b) {
             rest.push(arguments[i]);
         }
     }
-    console.log('a = ')
+    console.log('a = ' + a);
+    console.log('b = ' + b);
+    console.log(rest);
+}
+/*
+为了获取除了已定义参数a、b之外的参数，我们不得不用arguments，
+并且循环要从索引2开始以便排除前两个参数，这种写法很别扭，
+只是为了获得额外的rest参数。
+
+
+更好的方法是：
+ES6标准引入了rest参数，上面的函数可以改写为：
+*/
+function foo(a, b, ...rest) { // rest参数只能写在最后，前面要用“...”标识
+    console.log('a = ' + a);
+    console.log('b = ' + b);
+    console.log(rest);
+}
+
+foo(1, 2, 3, 4, 5);
+// 结果：
+// a = 1
+// b = 2
+// Array [ 3, 4, 5 ]
+
+foo(1);
+// a = 1
+// b = undefined
+// Array []
+/*
+从运行结果可知，传入的参数先绑定a、b，多余的参数以数组形式交给变量rest，
+所以，不再需要arguments我们就获取了全部参数。
+
+如果传入的参数连正常定义的参数都没填满，也不要紧，rest参数会接受一个空数组（注意不是undefined）。
+*/
+
+
+// --------------------------------------------------
+
+/* 因为rest参数是ES6新标准，所以需要测试一下浏览器是否支持。
+
+请用rest参数编写一个sum()函数，接受任意个参数并返回它们的和：*/
+'use strict';
+function sum(...rest) {
+    ???
 }
